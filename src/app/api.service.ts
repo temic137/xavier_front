4ce8ef19a9021cc69c37caf30b2875c28bb1ef37
temic137 <68796483+temic137@users.noreply.github.com>
2025-02-25 22:32:27 +0100

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -65,7 +65,7 @@ export class ApiService {
   // private apiUrl = 'https://xavier-ai-backend.onrender.com'; 
 
   
-  private apiUrl = 'http://localhost:5000';
+  private apiUrl = 'https://xavier-back.onrender.com';
   
    
   constructor(private http: HttpClient) { }
