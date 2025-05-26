32a43ab9897c8dc9e82a96dcb655d4b9846ed1dd
temic137 <68796483+temic137@users.noreply.github.com>
2025-05-26 02:45:38 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -123,7 +123,20 @@
 
 
 
-
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ChatbotListComponent } from './chatbot-list/chatbot-list.component';
+import { ChatbotChatComponent } from './chatbot-chat/chatbot-chat.component';
+import { ChatbotEditComponent } from './chatbot-edit/chatbot-edit.component';
+import { GmailIntegrationComponent } from './gmail-integration/gmail-integration.component';
+import { LandingComponent } from './landing/landing.component';
+import { AnalyticsDashboardComponent } from './analytics-dashboard/analytics-dashboard.component';
+import { ChatbotDetailComponent } from './chatbot-detail/chatbot-detail.component';
+import { TrainComponent } from './train/train.component';
+import { IntegrationComponent } from './integration/integration.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FeedbackDashboardComponent } from './feedback-dashboard/feedback-dashboard.component';
 import { ContactComponent } from './contact/contact.component';
 import { CareersComponent } from './careers/careers.component';
 import { AboutComponent } from './about/about.component';
